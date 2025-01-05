1d1b244086b384d5377aac0501ca89d36f159fdf
Qiting Wu <qitingwu5578@gmail.com>
2025-01-05 10:02:56 -0500

    initial commit

---
+++ src/test/java/com/activiti/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activiti.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
